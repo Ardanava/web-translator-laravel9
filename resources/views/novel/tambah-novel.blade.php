0308464c8a8860757f08cde37b67efcbf72472b6
osirusophiuchus16@gmail.com <galihnulhakim1604@gmail.com>
2023-01-14 10:57:47 +0700

    Commit Kedua

--- resources/views/novel/tambah-novel.blade.php
+++ resources/views/novel/tambah-novel.blade.php
@@ -71,7 +71,7 @@
                                   <div class="relative w-full mb-3">
                                     <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
                                       Sampul
-                                    </label>
+                                    </label> 
                                     <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file">
                                   </div>
                                 </div>
